5f9a4885237c353912c55c582c335d8aeeb68997
Anderson Morais Mann <anderson.mann@4all.com>
2019-01-08 19:30:04 -0200

    commit base page

---
+++ selenium-parallel-execution/src/test/java/core/BasePage.java
@@ -0,0 +1,56 @@
+/**
+ * 
+ */
+package core;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+/**
+ * @author anderson.mann
+ *
+ */
+public class BasePage extends PageObject {
+
+	public BasePage(WebDriver driver) {
+		super(driver);
+	}
+
+	public WebElement waitForElement(WebElement element) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.visibilityOf(element));
+			return element;
+		} catch (Exception e) {
+			throw new NoSuchElementException("Element not found: " + e.getMessage());
+		}
+	}
+
+	public void click(WebElement element) {
+		this.waitForElement(element).click();
+	}
+
+	public void write(WebElement element, String keys) {
+		this.waitForElement(element).sendKeys(keys);
+	}
+
+	public WebElement getText(WebElement text) {
+		this.waitForElement(text).getText();
+		return text;
+	}
+
+	public void escrever(By by, String text) {
+		try {
+			WebDriverWait wait = new WebDriverWait(driver, 10);
+			wait.until(ExpectedConditions.presenceOfElementLocated(by));
+			driver.findElement(by).sendKeys(text);
+		} catch (Exception e) {
+			throw new NoSuchElementException("Element not found: " + e.getMessage());
+		}
+	}
+
+}
