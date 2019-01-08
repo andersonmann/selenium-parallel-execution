1eb55c3db12b55c6786a1c4eaf98f7b8a6e23493
Anderson Morais Mann <anderson.mann@4all.com>
2019-01-08 19:35:22 -0200

    add class BaseTest

---
+++ selenium-parallel-execution/src/test/java/core/BaseTest.java
@@ -0,0 +1,29 @@
+/**
+ * 
+ */
+package core;
+
+/**
+ * @author anderson.mann
+ *
+ */
+
+public class BaseTest extends BrowserFactory {
+
+//	public static WebDriver driver;
+//	public GoogleHomePage googleHome;
+
+//	@BeforeClass
+//	public void setup() {
+//		googleHome = new GoogleHomePage(driver);
+//	}
+
+//	@AfterMethod
+//	public static void killDriver() {
+//		if (driver != null) {
+//			driver.quit();
+//			driver = null;
+//		}
+//	}
+
+}
